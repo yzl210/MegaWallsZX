c4aa1a2782225247b19d94f4700df908e7080021
yzl210 <2846840578@qq.com>
2020-08-03 13:01:38 +0800

    Initial commit

---
+++ src/main/java/cn/leomc/megawallszx/MegaWallsZX.java
@@ -0,0 +1,16 @@
+package cn.leomc.megawallszx;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MegaWallsZX extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
